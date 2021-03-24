669487794c111dd75fab93051c5359b68d584319
Matt Carter <carter.matthew@me.com>
2021-03-23 22:07:51 -0400

    update

--- client/src/components/siteWide/Portal.js
+++ client/src/components/siteWide/Portal.js
@@ -1,38 +1,54 @@
-import { Button, Container, Paper } from "@material-ui/core"
+import { Grid, Paper, Button, Box } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles( theme => ({
-    container: {
+    root: {
         height: '60vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center'
     },
+    gridItemTop: {
+    },
     paper: {
+        height: '400px',
         display: 'flex',
         flexDirection: 'column',
-        justifyContent: 'space-around',
         padding: '20px'
     },
-    button: {
-        width: '20rem',
-        alignSelf: 'center'
+    box: {
+        alignSelf: 'center',
+        backgroundColor: '#3f51b5',
+        color: 'white',
+        width: '200px',
+        textAlign: 'center'
     }
 }))
+
+{/* <Paper className={classes.paper}>
+<Button className={classes.button} variant='contained' color='primary'>
+    Public
+</Button>
+<Button className={classes.button} variant='contained' color='primary'>
+    Agency
+</Button>
+</Paper> */}
 
 const Portal = () => {
     const classes = useStyles()
     return (
-        <Container className={classes.container} maxWidth='sm'>
-            <Paper className={classes.paper}>
-                <Button className={classes.button} variant='contained' color='primary'>
-                    Public
-                </Button>
-                <Button className={classes.button} variant='contained' color='primary'>
-                    Agency
-                </Button>
-            </Paper>
-        </Container>
+        <Grid container className={classes.root}>
+            <Grid item md={5} className={classes.gridItemTop}>
+                <Paper className={classes.paper}>
+                    <Box className={classes.box}>
+                        Public
+                    </Box>
+                    <Box className={classes.box}>
+                        Agency
+                    </Box>
+                </Paper>
+            </Grid>
+        </Grid>
     )
 }
 
